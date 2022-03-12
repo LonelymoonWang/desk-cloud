7f769c5a72ad30103ff803a3cd7691419e9c4c65
lonelymoon <lonelymoon@aliyun.com>
2022-03-12 12:16:15 +0800

    新建网关服务

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # desk-cloud
 微服务项目
+
+技术选型：
+SpringCloud
+SpringBoot
+SpringCloudGateway
+SpringSecurity(Sa-Token)
+SpringDataJpa(MybatisPlus)
+SpringBootRedis
+SpringCloudConfig
---
+++ desk-gateway/src/main/java/wang/lonelymoon/desk/gateway/DeskGatewayApplication.java
@@ -0,0 +1,16 @@
+package wang.lonelymoon.desk.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lonelymoon
+ */
+@SpringBootApplication
+public class DeskGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskGatewayApplication.class, args);
+    }
+
+}
