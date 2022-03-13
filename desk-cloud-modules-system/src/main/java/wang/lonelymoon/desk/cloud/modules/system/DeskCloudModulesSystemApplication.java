49392818ce867a341f2bdfccae63b070dba1eaf8
lonelymoon <lonelymoon@aliyun.com>
2022-03-13 15:29:05 +0800

    system 模块添加jpa配置

--- desk-cloud-modules-system/src/main/java/wang/lonelymoon/desk/cloud/modules/system/DeskCloudModulesSystemApplication.java
+++ desk-cloud-modules-system/src/main/java/wang/lonelymoon/desk/cloud/modules/system/DeskCloudModulesSystemApplication.java
@@ -2,12 +2,21 @@ package wang.lonelymoon.desk.cloud.modules.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author lonelymoon
+ */
 @SpringBootApplication
+@RestController
 public class DeskCloudModulesSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DeskCloudModulesSystemApplication.class, args);
     }
 
+    @org.springframework.web.bind.annotation.RequestMapping
+    public String getStr(){
+        return "Hello";
+    }
 }
