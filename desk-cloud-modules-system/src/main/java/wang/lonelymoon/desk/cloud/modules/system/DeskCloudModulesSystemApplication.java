2f97024a2b4a4bbae72c5f158d736a538d37594f
lonelymoon <lonelymoon@aliyun.com>
2022-03-13 15:17:11 +0800

    新建system模块代码

---
+++ desk-cloud-modules-system/src/main/java/wang/lonelymoon/desk/cloud/modules/system/DeskCloudModulesSystemApplication.java
@@ -0,0 +1,13 @@
+package wang.lonelymoon.desk.cloud.modules.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeskCloudModulesSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeskCloudModulesSystemApplication.class, args);
+    }
+
+}
---
+++ desk-cloud-modules-system/src/test/java/wang/lonelymoon/desk/cloud/modules/system/DeskCloudModulesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package wang.lonelymoon.desk.cloud.modules.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskCloudModulesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
